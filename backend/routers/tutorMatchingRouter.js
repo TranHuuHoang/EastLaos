c3132eb1c3bfafd3b359651aa1a6f79ca7066bd8
duke-alibubu <ttdungalibubu@gmail.com>
2020-01-15 10:58:06 +0800

    template for quiz and tutor router

--- backend/routers/tutorMatchingRouter.js
+++ backend/routers/tutorMatchingRouter.js
@@ -2,22 +2,23 @@ const express = require('express')
 const router = new express.Router()
 const auth = require('../middleware/auth')
 
-router.get('/course_details', auth, async(req, res) => {
+router.get('/match', auth, async (req, res) => {
     try {
 
-    }
-    catch (e) {
-
-    }
-})
-
-router.post('/courses', auth, async(req, res) => {
-    try {
-        
     }
     catch (e){
 
     }
 })
+
+router.get('/subject', auth, async (req, res) => {
+    try {
+
+    }
+    catch (e){
+
+    }
+})
+
 
 module.exports = router
