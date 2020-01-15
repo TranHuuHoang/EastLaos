917ce0f508e20c09a93464211f45034d85d98086
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-01-15 13:34:27 +0800

    added session test template

--- backend/src/db/test.js
+++ backend/src/db/test.js
@@ -7,20 +7,16 @@ const session = new Session();
 const file = readFilePromise("./src/db/schema.sql")
 
 const test = async function() {
-  try {
-    const filedata = await file;
-    filedata.toString().split(";").forEach(async function(line) {
-      const out = await session.executeSQL(line);
-      console.log(line)
-      console.log(out);
-    });
-  } catch (e) {
-    return e
-  }
+  const filedata = await file;
+  filedata.toString().split(";").forEach(async function(line) {
+    const out = await session.executeSQL(line);
+    console.log(line)
+    console.log(out);
+  });
 }
 
-test().then(function(data) {
-}).catch(function(err) {
+test().catch(function(err) {
+  throw err
 })
 
 
