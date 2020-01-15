c3a9b2601bd79c7e8a9c05e2e64073814ef445a8
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-01-15 17:44:28 +0800

    updated session test

--- backend/src/db/session.js
+++ backend/src/db/session.js
@@ -16,25 +16,27 @@ const Session = class {
     this.session = mysqlx.getSession(options)
   }
   async executeSQL(text) {
-    try {
-      const session = await this.session;
-      const result = await session.sql(text).execute();
-      return result.fetchAll();
-    } catch (e) {
-      return e
-    }
+    const session = await this.session;
+    const result = await session.sql(text).execute();
+    return result.fetchAll();
   }
   // student
   async createStudent(id, email, password, salt) {
     return this.executeSQL(
       `INSERT INTO student
-      VALUES (${id}, ${email}, ${password}, ${salt})`
+      VALUES (${id}, "${email}", "${password}", "${salt}")`
     );
+  }
+  async deleteStudent(id) {
+    return this.executeSQL(
+      `DELETE FROM student
+      WHERE id = ${id}`
+    )
   }
   async updateStudentPassword(id, password, salt) {
     return this.executeSQL(
       `UPDATE student
-      SET password = ${password}, salt = ${salt}
+      SET password = "${password}", salt = "${salt}"
       WHERE id = ${id}`
     );
   }
@@ -42,27 +44,6 @@ const Session = class {
     return this.executeSQL(
       `SELECT *
       FROM student
-      WHERE id = ${id}`
-    );
-  }
-  // tutor
-  async createTutor(id, email, password, salt) {
-    return this.executeSQL(
-      `INSERT INTO tutor
-      VALUES (${id}, ${email}, ${password}, ${salt})`
-    )
-  }
-  async updateTutorPassword(id, password, salt) {
-    return this.executeSQL(
-      `UPDATE tutor
-      SET password = ${password}, salt = ${salt}
-      WHERE id = ${id}`
-    );
-  }
-  async findtutorById(id) {
-    return this.executeSQL(
-      `SELECT *
-      FROM tutor
       WHERE id = ${id}`
     );
   }
