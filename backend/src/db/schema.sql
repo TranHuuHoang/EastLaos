50167b8b4d6d9fa01c9495f1c8576b919ba19d2e
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-01-15 12:29:00 +0800

    schema new

--- backend/src/db/schema.sql
+++ backend/src/db/schema.sql
@@ -1,53 +1,64 @@
 USE EastLaos;
 
--- PRIMITIVE
+DROP TABLE IF EXISTS matching;
+DROP TABLE IF EXISTS tutormatch;
+DROP TABLE IF EXISTS studentmatch;
+DROP TABLE IF EXISTS mark;
+
+DROP TABLE IF EXISTS quiz;
 DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS tutor;
+DROP TABLE IF EXISTS course;
+
+-- PRIMITIVE
 CREATE TABLE student (
   id        int(32) NOT NULL,
   email     varchar(256) NOT NULL,
   password  varchar(256) NOT NULL,
-  CONSTRAINT uaStudent UNIQUE(id, email)
+  CONSTRAINT uaStudentID UNIQUE(id),
+  CONSTRAINT uaStudentEmail UNIQUE(email)
+
 );
 
-DROP TABLE IF EXISTS tutor;
 CREATE TABLE tutor (
   id        int(32) NOT NULL,
   email     varchar(256) NOT NULL,
   password  varchar(256) NOT NULL,
-  CONSTRAINT uaTutor UNIQUE(id, email)
+  CONSTRAINT uaTutorId UNIQUE(id),
+  CONSTRAINT uaTutorEmail UNIQUE(email)
 );
 
-DROP TABLE IF EXISTS course;
 CREATE TABLE course (
   id        int(32) NOT NULL,
   code      varchar(8) NOT NULL,
   name      varchar(256) NOT NULL,
-  CONSTRAINT uaCourse UNIQUE(id, code)
+  CONSTRAINT uaCourseId UNIQUE(id),
+  CONSTRAINT uaCourseCode UNIQUE(code)
 );
 
 -- MATCHING
 
-DROP TABLE IF EXISTS studentmatch;
 CREATE TABLE studentmatch (
   studentId int(32),
   courseId  int(32),
+  CONSTRAINT uaStudentMatch UNIQUE(studentId, courseId),
   FOREIGN KEY (studentId) REFERENCES student(id),
   FOREIGN KEY (courseId) REFERENCES course(id)
 );
 
-DROP TABLE IF EXISTS tutormatch;
 CREATE TABLE tutormatch (
   tutorId   int(32),
   courseId  int(32),
+  CONSTRAINT uaTutorMatch UNIQUE(tutorId, courseId),
   FOREIGN KEY (tutorId) REFERENCES tutor(id),
   FOREIGN KEY (courseId) REFERENCES course(id)
 );
 
-DROP TABLE IF EXISTS matching;
 CREATE TABLE matching (
   studentId int(32),
   tutorId   int(32),
   courseId  int(32),
+  CONSTRAINT uaMatching UNIQUE(studentId, tutorId, courseId),
   FOREIGN KEY (studentId) REFERENCES student(id),
   FOREIGN KEY (tutorId) REFERENCES tutor(id),
   FOREIGN KEY (courseId) REFERENCES course(id)
@@ -55,19 +66,19 @@ CREATE TABLE matching (
 
 -- QUIZ
 
-DROP TABLE IF EXISTS quiz;
 CREATE TABLE quiz (
   id        int(32) NOT NULL,
   courseId  int(32),
   content   varchar(1024),
-  CONSTRAINT uaQuiz UNIQUE(id)
+  CONSTRAINT uaQuiz UNIQUE(id),
+  FOREIGN KEY (courseId) REFERENCES course(id)
 );
 
-DROP TABLE IF EXISTS mark;
 CREATE TABLE mark (
   quizId    int(32),
   studentId int(32),
   mark      int(32),
+  CONSTRAINT uaMark UNIQUE(quizId, studentId),
   FOREIGN KEY (quizId) REFERENCES quiz(id),
   FOREIGN KEY (studentId) REFERENCES student(id)
 );
