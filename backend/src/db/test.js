57fff8b6f9cb6e3e1b68309395b7fd65459005e1
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-01-15 16:55:14 +0800

    updated session test

--- backend/src/db/test.js
+++ backend/src/db/test.js
@@ -4,25 +4,22 @@ const {promisify} = require("util")
 const readFilePromise = promisify(readFile) 
 
 const session = new Session();
-const schemaFile = readFilePromise("./src/db/schema.sql");
-const testFile = readFilePromise("./src/db/test.sql");
 
-const test = async function() {
-  const schemaData = await schemaFile;
-  schemaData.toString().split(";").forEach(async function(line) {
+const execFile = async function(filename) {
+  const data = (await readFilePromise(filename)).toString().split(";");
+  for (i in data) {
+    const line = data[i];
     if (line.length > 0) {
       const out = await session.executeSQL(line);
-      console.log(line)
+      console.log(line);
       console.log(out);
     }
-  });
+  }
+};
+const test = async function() {
+  await execFile("./src/db/schema.sql");
+  await execFile("./src/db/test.sql");
+  console.log("Done!");
 }
 
-test().catch(function(err) {
-  throw err
-})
-
-
-console.log()
-
-
+test();
