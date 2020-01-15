776408a6afe5c647126f105ef94bb5d807e42a43
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-01-15 18:52:16 +0800

    nguhoc

--- backend/src/db/test.js
+++ backend/src/db/test.js
@@ -4,19 +4,24 @@ const {promisify} = require("util")
 const readFilePromise = promisify(readFile) 
 
 const execFile = async function(filename) {
-  const data = (await readFilePromise(filename)).toString().split(";");
-  for (i in data) {
-    const line = data[i];
-    if (line.length > 0) {
-      try {
-        const out = await session.executeSQL(line);
-        console.log(line);
-        console.log(out);
-      } catch (e) {
-        console.log(line);
-        console.log(e.info)
+  try {
+    const data = (await readFilePromise(filename)).toString().split(";");
+    for (i in data) {
+      const line = data[i];
+      if (line.length > 0) {
+        try {
+          const out = await session.executeSQL(line);
+          console.log(line);
+          console.log(out);
+        } catch (e) {
+          console.log(line);
+          console.log(e.info)
+        }
       }
     }
+  }
+  catch (e) {
+    return e;
   }
 };
 const test = async function() {
