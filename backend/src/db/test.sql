1a775b946c2945b05c9fabfd5c778a204be87a54
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-01-15 16:59:23 +0800

    updated session test

---
+++ backend/src/db/test.sql
@@ -0,0 +1,12 @@
+INSERT INTO student
+VALUES (1, "1@test", "caascasca", 123123);
+
+INSERT INTO student
+VALUES (2, "2@test", "caascasca", 123123);
+
+SELECT *
+FROM student
+WHERE id = 1;
+
+INSERT INTO student
+VALUES (3, "1@test", "caascasca", 123123);
