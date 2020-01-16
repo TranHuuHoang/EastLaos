8f0c17942262d3eb30034310a966c6ec81ff7419
duke-alibubu <ttdungalibubu@gmail.com>
2020-01-16 11:38:42 +0800

    Log in func

--- backend/src/utils/pwdHashSalt.js
+++ backend/src/utils/pwdHashSalt.js
@@ -3,8 +3,9 @@ const crypto = require('crypto')
 const generateRandomSalt = (length) => crypto.randomBytes(Math.ceil(length/2))
                                     .toString('hex') /** convert to hexadecimal format */
                                     .slice(0,length)
-const sha512 = (password) => {
-    const salt = generateRandomSalt(16)
+const sha512 = (password, salt = -1) => {
+    if (salt == -1)
+        salt = generateRandomSalt(16)
     const hash = crypto.createHmac('sha512', salt)
     hash.update(password)
     const hashedValue = hash.digest('hex');
