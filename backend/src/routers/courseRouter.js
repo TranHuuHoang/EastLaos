91d7b07e50ac688418465c5273cfeac8c97ef2fd
duke-alibubu <ttdungalibubu@gmail.com>
2020-01-16 14:56:11 +0800

    temporarily remove course admin actions

--- backend/src/routers/courseRouter.js
+++ backend/src/routers/courseRouter.js
@@ -21,39 +21,39 @@ router.get('/course_details/:course_code', auth, async(req, res) => {
     }
 })
 
-router.post('/courses', auth, async(req, res) => {
-    try {
-        let counter = await dbSession.maxCourseId()
-        if (!counter){
-            counter = 1
-        }
-        else {
-            counter++
-        }
-        await dbSession.createCourse({
-            id: counter,
-            code: req.body.code,
-            name: req.body.name,
-            info: req.body.info
-        })
-        const courseCreated = await dbSession.findCourseById(counter)
-        if (!courseCreated){
-            throw new Error("Course not created!")
-        }
-        res.send(courseCreated)
-    }
-    catch (e){
-        res.status(500).send(e.message)
-    }
-})
+// router.post('/courses', auth, async(req, res) => {
+//     try {
+//         let counter = await dbSession.maxCourseId()
+//         if (!counter){
+//             counter = 1
+//         }
+//         else {
+//             counter++
+//         }
+//         await dbSession.createCourse({
+//             id: counter,
+//             code: req.body.code,
+//             name: req.body.name,
+//             info: req.body.info
+//         })
+//         const courseCreated = await dbSession.findCourseById(counter)
+//         if (!courseCreated){
+//             throw new Error("Course not created!")
+//         }
+//         res.send(courseCreated)
+//     }
+//     catch (e){
+//         res.status(500).send(e.message)
+//     }
+// })
 
-router.delete('/courses', auth, async(req, res) => {
-    try {
+// router.delete('/courses', auth, async(req, res) => {
+//     try {
         
-    }
-    catch (e){
+//     }
+//     catch (e){
 
-    }
-})
+//     }
+// })
 
 module.exports = router
