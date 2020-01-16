81c9105491667eeef42d7ed2382e89cc27dc3311
duke-alibubu <ttdungalibubu@gmail.com>
2020-01-16 11:19:32 +0800

    Add SQL injection

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,13 @@ const courseRouter = require('./routers/courseRouter')
 const matchingRouter = require('./routers/matchingRouter')
 const quizRouter = require('./routers/quizRouter')
 const tutorRouter = require('./routers/tutorRouter')
+const sqlinjection = require('sql-injection')
 
 const app = express()
 
 //parse incoming JSON as object so we can access it in our request handler.
+app.use(sqlinjection)
+
 app.use(express.json())
 app.use(studentRouter)
 app.use(courseRouter)
