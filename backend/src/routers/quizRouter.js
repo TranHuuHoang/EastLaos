87dbd51aa4de59c3258dfcfbab3ff995507a4075
duke-alibubu <ttdungalibubu@gmail.com>
2020-01-16 16:51:59 +0800

    template for quiz routers

--- backend/src/routers/quizRouter.js
+++ backend/src/routers/quizRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = new express.Router()
 const auth = require('../middleware/auth')
+const {dbSession} = require('../db/session')
 
 router.get('/quiz_mark', auth, async (req, res) => {
     try {
@@ -20,5 +21,18 @@ router.get('/leaderboard', auth, async (req, res) => {
     }
 })
 
+router.get('/quiz/:course_code', auth, async (req, res) => {
+    try {
+        // const course = await dbSession.findCourseByCode(req.params.course_code)
+        // if (!course){
+        //     throw new Error('Wrong course code!')
+        // }
+        // const quiz = await dbSession.createQuiz(course.code)
+        // res.send(quiz) 
+    }
+    catch (e) {
+        res.status(500).send(e.message)
+    }
+})
 
 module.exports = router
