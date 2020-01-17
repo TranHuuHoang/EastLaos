4dbf6f566bfe2eb62d1b4fd9d7a2576942ae15e7
Hoang Nghia Tuyen <hnt4499@gmail.com>
2020-01-17 18:08:51 +0800

    Add redirecting and parsing data from course_list.html

--- frontend/resources/js/course_info.js
+++ frontend/resources/js/course_info.js
@@ -1,3 +1,13 @@
 // Get course data
-var courseData = sessionStorage.getItem("courseData")
-console.log(JSON.parse(courseData));
+var courseData = JSON.parse(sessionStorage.getItem("courseData"))
+// console.log(JSON.parse(courseData));
+
+// $("#courseName").html(courseData["name"]);
+// $("#courseCode").html(courseData["code"]);
+// $("#courseInfo").html(courseData["information"]);
+
+$(document).ready(function() {
+  $("#courseName").html(courseData["name"]);
+  $("#courseCode").html(courseData["code"]);
+  $("#courseInfo").html(courseData["information"]);
+});
